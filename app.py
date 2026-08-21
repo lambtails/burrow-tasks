6ff8d317969db37b9d8392d781c5eaef3a5f4c23
Bunny <167683926+s4105951@users.noreply.github.com>
2026-08-21 20:34:01 +1000

    add dragging logic, timestamps, cleaner db access

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request
 import sqlite3
+import datetime
 
 # Create database connection
 db = "database.db"
-connect = sqlite3.connect(db)
-connect.execute("""
-    CREATE TABLE IF NOT EXISTS tasks (
-    rowid INTEGER NOT NULL PRIMARY KEY,
-    description TEXT NOT NULL,
-    done BOOL NOT NULL,
-    archived BOOL NOT NULL,
-    deleted BOOL NOT NULL
-    )
-    """)
+detect_types = sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+
+# Generate a table if none exists
+with sqlite3.connect(db, detect_types=detect_types) as connect:
+    connect.execute("""
+        CREATE TABLE IF NOT EXISTS Tasks (
+        RowID INTEGER NOT NULL PRIMARY KEY,
+        Description TEXT NOT NULL,
+        Done BOOL NOT NULL,
+        Archived BOOL NOT NULL,
+        Deleted BOOL NOT NULL,
+        CreationDate TIMESTAMP,
+        DoneDate TIMESTAMP,
+        ArchiveDate TIMESTAMP,
+        DeletionDate TIMESTAMP
+        )
+        """)
 
 # Start flask app and define URL routes
 app = Flask(__name__)
@@ -23,26 +31,28 @@ def index():
     if request.method == "POST":
         # Add new task to the database!
         description = request.form.get("description")
-        with sqlite3.connect(db) as users:
-            cursor = users.cursor()
+        current_datetime = datetime.datetime.now()
+        with sqlite3.connect(db, detect_types=detect_types) as connect:
+            cursor = connect.cursor()
             cursor.execute(
                 """
-                INSERT INTO tasks \
-                    (description,done,archived,deleted) VALUES (?,?,?,?)
+                INSERT INTO Tasks \
+                    (Description,Done,Archived,Deleted,CreationDate) VALUES (?,?,?,?,?)
                 """,
-                (description, False, False, False),
+                (description, False, False, False, current_datetime),
             )
-            users.commit()
+            connect.commit()
+            cursor.close()
 
         # Redirect to a GET request so the browser doesn't redo stuff
         return app.redirect(app.url_for(endpoint="index"))
 
     # Connect and load database for GET request
-    connect = sqlite3.connect(db)
-    cursor = connect.cursor()
-    cursor.execute("SELECT * FROM tasks")
-    data = cursor.fetchall()
-    connect.close()
+    with sqlite3.connect(db, detect_types=detect_types) as connect:
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM Tasks ORDER BY Done ASC, DoneDate DESC, CreationDate ASC;")
+        data = cursor.fetchall()
+        cursor.close()
 
     # Render webpage
     return render_template("index.html", data=data)
@@ -51,26 +61,30 @@ def index():
 @app.route("/update", methods=["POST"])
 def update():
     # Get data from the JSON request
-    rowid: int = int(request.json.get("rowid"))
+    row_id: int = int(request.json.get("row_id"))
     done: bool | None = request.json.get("done")
+    done_date: datetime.datetime | None = None
+
+    if done == True:
+        done_date = datetime.datetime.now()
 
     # Update the database
-    connect = sqlite3.connect(db)
-    cursor = connect.cursor()
-    cursor.execute(
-        """
-        UPDATE tasks \
-        SET done=? \
-        WHERE rowid=?
-        """,
-        (done, rowid),
-    )
-    connect.commit()
-    connect.close()
+    with sqlite3.connect(db, detect_types=detect_types) as connect:
+        cursor = connect.cursor()
+        cursor.execute(
+            """
+            UPDATE Tasks
+            SET Done=?, DoneDate=?
+            WHERE RowID=?
+            """,
+            (done, done_date, row_id),
+        )
+        connect.commit()
+        cursor.close()
 
     # Return empty string for now
     return ""
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host="0.0.0.0")
